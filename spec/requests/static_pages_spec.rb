c3a40e3193d6d6a7e8f4901125a9905761ea428e
Eduardo Gonzalez <eduardog@telenav.com>
2013-07-19 18:16:17 -0700

    Add tests on the links and the full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,22 @@ describe "StaticPages" do
     let(:page_title) { 'Contact' }
     it_should_behave_like "all static pages"
   end
+
+  describe "Static pages" do
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', text: full_title('About Us')
+      click_link "Help"
+      page.should have_selector 'title', text: full_title('Help')
+      click_link "Contact"
+      page.should have_selector 'title', text: full_title('Contact')
+      click_link "Home"
+      page.should have_selector 'title', text: full_title('')
+      click_link "Sign up now!"
+      page.should have_selector 'title', text: full_title('Sign up')
+      click_link "sample app"
+      page.should have_selector 'title', text: full_title('')
+    end
+  end
 end
